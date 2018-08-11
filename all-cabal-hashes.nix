c8aaf1466d7d7fe7d42e7fa9b332ab7ff721b305
Matthew Pickering <matthewtpickering@gmail.com>
2018-08-11 08:24:47 +0000

    Update all-cabal-hashes

--- all-cabal-hashes.nix
+++ all-cabal-hashes.nix
@@ -1,6 +1,6 @@
-let rev = "e8e76bc26a994aee313e571dc4e6701398d17a42";
+let rev = "f7cb14627b80fed741b55ddd30fd1df3c9cf861c";
 in
 {
   url    = "https://github.com/commercialhaskell/all-cabal-hashes/archive/${rev}.tar.gz";
-  sha256 = "16rnyxqmr93ahml0fjfa6hmjpmx8sbpfdr52krd2sd6ic9n5p5ix";
+  sha256 = "1vqffgi7jr3zkir1kp59965a579zf561vriba2b7kafrkr9jg3bn";
 }
